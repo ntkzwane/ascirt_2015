a10b4b55ae07064817324ffa262cfbbe4bbe9832
Jae Jang <jngjae001@snoek.cs.uct.ac.za>
2015-10-17 15:45:37 +0200

    changed configuration

--- hons-controller-master/NEAT_controller_script_medium.sh
+++ hons-controller-master/NEAT_controller_script_medium.sh
@@ -1,11 +1,11 @@
 cd ~/git-repos/ascirt*/hons-controller*/
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test1.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test2.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test3.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test4.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test5.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test6.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test7.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test8.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test9.txt;
-java -jar build/libs/hons-controller-master-all.jar --control --environment medium_advanced < mediumconf > ~/testing/medium_test10.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test1.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test2.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test3.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test4.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test5.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test6.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test7.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test8.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test9.txt;
+java -jar build/libs/hons-controller-master-all.jar --control --environment m_a < mediumconf > ~/testing/medium_test10.txt;
